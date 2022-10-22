00232a2faf676f7e7b34a9d8ea36e0d6600fa4bf
z7z_Eta <1427294883@qq.com>
2022-10-22 22:19:25 +0800

    wasm mermaid svg feature

---
+++ dp.cpp
@@ -0,0 +1,93 @@
+#include"cwasm"
+#include<emscripten/emscripten.h>
+#include<cstdio>
+#include<algorithm>
+#include<sstream>
+#include<vector>
+#define rep(i,s,t) for(int i=(s);i<=(t);i++)
+#define rev_rep(i,s,t) for(int i=(s);i>=(t);i--)
+using ll = long long;
+using namespace std;
+stringstream ssin;
+ll ci(){ ll x; ssin>>x; return x; }
+
+int RET[1023];
+void output(int x){
+	if( RET[0]<1000 ) RET[++RET[0]] = x;
+}
+
+int a[103][103];
+int f[103][103];
+int from[103][103];
+
+EM_PORT_API(int*) triangle(const char*input){
+	ssin = stringstream(input);
+	int n = ci();
+	rep(i,1,n) rep(j,1,i) a[i][j] = ci();
+	rev_rep(i,n,1){
+		rep(j,1,i){
+			f[i][j] = max(f[i+1][j], f[i+1][j+1]) + a[i][j];
+			from[i][j] = f[i+1][j]<f[i+1][j+1];
+		}
+	}
+	RET[0] = 0;
+	output(n);
+	rep(i,1,n) rep(j,1,i) output(f[i][j]);
+	rep(i,1,n) rep(j,1,i) output(from[i][j]);
+	return RET;
+}
+/*
+(function (s){
+  let ptr = Module.ccall('triangle', 'number', ['string'], [s]);
+  let n = Module.HEAP32[ptr/4];
+  let a = [];
+  for(let i=1; i<=n; i++) a.push(Module.HEAP32[ptr/4+i]);
+  return a;
+})('3 3 2 1 3 4 6');
+'5 7 3 8 8 1 0 2 7 4 4 4 5 2 6 5'
+*/
+
+bool vis[103][103];
+
+const int dx[] = {0,-1,0,1};
+const int dy[] = {1,0,-1,0};
+
+int _n,_m;
+int dfs(int x, int y){
+	if( vis[x][y] ) return f[x][y];
+	vis[x][y] = 1;
+	rep(i,0,3){
+		int tx = x+dx[i], ty = y+dy[i];
+		if( 1<=tx && tx<=_n && 1<=ty && ty<=_m && a[tx][ty]<a[x][y] ){
+			int tmp = dfs(tx, ty)+1;
+			if( f[x][y]<tmp ) f[x][y]=tmp, from[x][y]=i;
+		}
+	}
+	return f[x][y];
+}
+
+EM_PORT_API(int*) ski(const char*input){
+	ssin = stringstream(input);
+	int n = ci(), m = ci();
+	_n = n;
+	_m = m;
+	rep(i,1,n) rep(j,1,m) a[i][j] = ci();
+	rep(i,1,n) rep(j,1,m) from[i][j] = -1;
+	rep(i,1,n) rep(j,1,m) f[i][j] = 1;
+	rep(i,1,n) rep(j,1,m) dfs(i,j);
+	RET[0] = 0;
+	output(n), output(m);
+	rep(i,1,n) rep(j,1,m) output(f[i][j]);
+	rep(i,1,n) rep(j,1,m) output(from[i][j]);
+	return RET;
+}
+/*
+(function (s){
+  let ptr = Module.ccall('ski', 'number', ['string'], [s]);
+  let n = Module.HEAP32[ptr/4];
+  let a = [];
+  for(let i=1; i<=n; i++) a.push(Module.HEAP32[ptr/4+i]);
+  return a;
+})('3 3 1 1 3 2 3 4 1 1 1');
+'5 5 1 2 3 4 5 16 17 18 19 6 15 24 25 20 7 14 23 22 21 8 13 12 11 10 9'
+*/
--- z.js
+++ z.js
@@ -1,35 +1,67 @@
-let triangleFirst = true;
-let lines = [];
+const $ = document.querySelectorAll.bind(document);
+
+function callModule(func, input){
+  let ptr = Module.ccall(func, 'number', ['string'], [input]);
+  let n = Module.HEAP32[ptr/4];
+  let a = [];
+  for(let i=1; i<=n; i++) a.push(Module.HEAP32[ptr/4+i]);
+  return a;
+}
+
 let gnodes = [null];
 function drawTriangle(input){
+  let oldsvg = $('#triangle > svg')[0];
+  if( oldsvg ) oldsvg.remove(), gnodes = [null];
   console.log(input);
   input = input.split(/\s+/).map(x=>parseInt(x));
-  let draw = SVG().addTo('#triangle').size('100%', '100%')
   let n = input[0];
+  let scale = 2*(30+30*n);
+  let draw = SVG().addTo('#triangle').size('100%', '100%').attr({viewBox:'0 0 '+scale+' '+scale});
   let tot = 0;
   for(let i=1; i<=n; i++){
     gnodes.push([null]);
     for(let j=1; j<=i; j++){
       let y = i*60, x = j*60 + (n-i)*30;
+      let lines = [null, null]
       if( i!=n ){
-        draw.line(0,0,-30,60).stroke({width:3,color:'#fff'}).dmove(x,y)
-        let line = draw.line(0,0,30,60).stroke({width:3,color:'#fff'}).dmove(x,y);
-        lines.push(line);
+        lines[0] = draw.line(0,0,-30,60).stroke({width:3,color:'#fff'}).dmove(x,y);
+        lines[1] = draw.line(0,0,30,60).stroke({width:3,color:'#fff'}).dmove(x,y);
       }
       var group = draw.group().fill('#111')
+      group.Line = lines
       group.circle(40).center(x,y).fill('lightsteelblue').stroke({width:2,color:'darkblue'})
       //group.path('M10,20L30,40').attr({'stroke-width': 2, stroke: '#333'})
-      group.text(input[++tot]).attr({x, y:y+7, 'font-size':20, 'text-anchor':'middle'})
+      group.Text = group.text(input[++tot]).attr({x, y:y+7, 'font-size':20, 'text-anchor':'middle'})
       gnodes[i].push(group);
     }
   }
-  console.log(gnodes);
-  lines.forEach(x=>x.animate(2000, 1000, 'now').stroke({color:'#f03'}))
 }
 
+function solveTriangle(input){
+  let oldsvg = $('#triangle > svg')[0];
+  if( !oldsvg ) drawTriangle(input);
+  let ans = callModule('triangle', input);
+  let n = ans[0];
+  let tot1 = n*(n+1)/2;
+  let tot2 = tot1*2 - n;
+  console.log(ans, tot1, tot2);
+  for(let i=n; i>=1; i--){
+    console.log(i);
+    if( i!=n ){
+      for(let j=i; j>=1; j--){
+        gnodes[i][j].Line[ans[tot2--]].animate(2000, 1000, 'now').stroke({color:'#f03'})
+      }
+    }
+    for(let j=i; j>=1; j--){
+      gnodes[i][j].fill('yellow').Text.text(ans[tot1--])
+    }
+  }
+}
+
+let triangleFirst = true;
 Reveal.on('triangle', () => {
   if( triangleFirst ){
-    drawTriangle(document.getElementById('triangle-input').value);
+    drawTriangle($('#triangle-input')[0].value);
     triangleFirst = false;
   }
 });
